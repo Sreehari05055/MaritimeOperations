3eb8d14b329e7bc4ade187be197accbf0882b1de
SREEPC\sreek <sreekuttankzm@gmail.com>
2025-02-15 17:59:21 +0000

    Adding model with openai reccomendations

--- pred_ship.py
+++ pred_ship.py
@@ -1,5 +1,11 @@
+import json
 import pickle
+import openai
 import pandas as pd
+
+with open("secrets.json", 'r') as f:
+    config = json.load(f)
+openai.api_key = config['openai']['api_key']
 
 with open("model/ship_co2.pkl", "rb") as file:
     model = pickle.load(file)
@@ -20,22 +26,25 @@ with open("model/scaler_fuel.pkl", "rb") as file:
 
 new_data = {
     'ship_type': ['Oil Service Boat'],
-    'fuel_type': ['Diesel'],
-    'distance': [58.9],
-    'fuel_consumption': [855.45],
-    'weather_conditions': ['Calm']
+    'fuel_type': ['HFO'],
+    'distance': [128.9],
+    'fuel_consumption': [4461.45],
+    'weather_conditions': ['Moderate'],
+    'engine_efficiency': [92]
 }
 new_data_oil = {
     'ship_type': ['Oil Service Boat'],
     'fuel_type': ['Diesel'],
     'distance': [23.9],
-    'weather_conditions': ['Moderate']
+    'weather_conditions': ['Moderate'],
+    'engine_efficiency': [86.98]
 }
 new_data_dist = {
     'ship_type': ['Oil Service Boat'],
     'fuel_type': ['Diesel'],
     'fuel_consumption': [734.45],
-    'weather_conditions': ['Calm']
+    'weather_conditions': ['Calm'],
+    'engine_efficiency': [3]
 }
 
 # Convert to DataFrame
@@ -44,23 +53,22 @@ new_data_co2 = pd.DataFrame(new_data)
 new_data_co2[['ship_type', 'fuel_type', 'weather_conditions']] = encoder.transform(
     new_data_co2[['ship_type', 'fuel_type', 'weather_conditions']])
 
-new_data_co2[['distance', 'fuel_consumption']] = scaler.transform(
-    new_data_co2[['distance', 'fuel_consumption']].values)
-
+new_data_co2[['distance', 'fuel_consumption', 'engine_efficiency']] = scaler.transform(
+    new_data_co2[['distance', 'fuel_consumption', 'engine_efficiency']].values)
 
 new_data_fuel = pd.DataFrame(new_data_oil)
 new_data_fuel[['ship_type', 'fuel_type', 'weather_conditions']] = encoder.transform(
     new_data_fuel[['ship_type', 'fuel_type', 'weather_conditions']])
 
-new_data_fuel[['distance']] = scaler_fuel.transform(
-    new_data_fuel[['distance']].values)
+new_data_fuel[['distance', 'engine_efficiency']] = scaler_fuel.transform(
+    new_data_fuel[['distance', 'engine_efficiency']].values)
 
 new_data_dist_pred = pd.DataFrame(new_data_dist)
 new_data_dist_pred[['ship_type', 'fuel_type', 'weather_conditions']] = encoder.transform(
     new_data_dist_pred[['ship_type', 'fuel_type', 'weather_conditions']]
 )
-new_data_dist_pred[['fuel_consumption']] = scaler_dist.transform(
-    new_data_dist_pred[['fuel_consumption']].values
+new_data_dist_pred[['fuel_consumption', 'engine_efficiency']] = scaler_dist.transform(
+    new_data_dist_pred[['fuel_consumption', 'engine_efficiency']].values
 )
 
 # Predict using the trained model
@@ -68,8 +76,49 @@ predicted_CO2 = model.predict(new_data_co2)
 predicted_fuel = model_fuel.predict(new_data_fuel)
 predicted_dist = model_dist.predict(new_data_dist_pred)
 
+"""
 
-# Display the predicted CO₂ emissions for the given input
+def recommendation_prompt(ship_data):
+    prompt = (f""
+            You are an assistant that recommends actions to optimize CO2 emissions Only based the provided factors.
+            here is the ship data:
+            -Distance Covered: {ship_data['distance']} Km
+            -Fuel Consumption: {ship_data['fuel_consumption']} Liters
+            -Weather Conditions {ship_data['weather_conditions']}
+            -Ship Type: {ship_data['ship_type']}
+            -Fuel Type: {ship_data['fuel_type']}
+            -Engine Efficiency: {ship_data['engine_efficiency']}
+            -Predicted CO2 Emissions {predicted_CO2[0]:.2f} Kg
+            You are explaining to a non-technical user, so first tell them the provided values and the predicted CO2 emissions, and then provide one line simple recommendations
+              "")
+
+    return openai_response(prompt)
+
+
+def openai_response(prompt):
+    try:
+        # Sending the prompt to the GPT-4 Turbo model for response
+        resp = openai.ChatCompletion.create(
+            model="gpt-4-turbo",
+            messages=[
+                {"role": "system", "content": "Respond concisely."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=200,  # Limit the number of tokens
+            temperature=0.3  # Controls response randomness; >0.4 for creativity, <0.4 for focus
+        )
+
+        return resp["choices"][0]["message"]["content"].strip()
+    except openai.OpenAIError as e:
+        return f"error: An error occurred with the AI service. Please try again later.{e}"
+    except Exception as e:
+        return F"An unexpected error occurred. Please try again later.{e}"
+
+
+# Display the predicted results for the given input
+print(recommendation_prompt(new_data))
+"""
 print(f"Fuel Consumption: {new_data['fuel_consumption'][0]} Liters --> Predicted CO₂ Emissions: {predicted_CO2[0]:.2f} Kg")
 print(f"Distance: {new_data_oil['distance'][0]} Km --> Predicted Fuel Consumption: {predicted_fuel[0]:.2f} Liters")
 print(f"Fuel Consumption: {new_data_dist['fuel_consumption'][0]} Liters --> Predicted Distance: {predicted_dist[0]:.2f} Km")
+
---
+++ train_ship.py
@@ -0,0 +1,132 @@
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
+
+# Load dataset
+df = pd.read_csv("highly_realistic_ship_fuel_efficiency.csv")
+
+X = pd.get_dummies(df, columns=['ship_type', 'fuel_type', 'weather_conditions'])
+
+# Define features and target
+X_dist = df[['ship_type', 'fuel_type', 'fuel_consumption', 'weather_conditions', 'engine_efficiency']]
+y_dist = df['distance']
+
+X_fuel = df[['ship_type', 'fuel_type', 'distance', 'weather_conditions', 'engine_efficiency']]
+y_fuel = df['fuel_consumption']
+
+X = df[['ship_type', 'fuel_type', 'distance', 'fuel_consumption', 'weather_conditions', 'engine_efficiency']]
+y = df['CO2_emissions']
+
+# Encode categorical variables using OrdinalEncoder
+encoder = OrdinalEncoder()
+encoder.fit(df[['ship_type', 'fuel_type', 'weather_conditions']])
+
+for dataset in [X_dist, X_fuel, X]:
+    dataset[['ship_type', 'fuel_type', 'weather_conditions']] = encoder.transform(
+        dataset[['ship_type', 'fuel_type', 'weather_conditions']]
+    )
+# Scale numerical features
+scaler_dist = MinMaxScaler()
+X_dist[['fuel_consumption', 'engine_efficiency']] = scaler_dist.fit_transform(X_dist[['fuel_consumption', 'engine_efficiency']].values)
+
+scaler_fuel = MinMaxScaler()
+X_fuel[['distance', 'engine_efficiency']] = scaler_fuel.fit_transform(X_fuel[['distance', 'engine_efficiency']].values)
+
+scaler = MinMaxScaler()
+X[['distance', 'fuel_consumption', 'engine_efficiency']] = scaler.fit_transform(
+    X[['distance', 'fuel_consumption', 'engine_efficiency']].values)
+
+X_dist_train, X_dist_test, y_dist_train, y_dist_test = train_test_split(X_dist, y_dist, test_size=0.2, random_state=42)
+X_fuel_train, X_fuel_test, y_fuel_train, y_fuel_test = train_test_split(X_fuel, y_fuel, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train models
+model_dist = RandomForestRegressor(n_estimators=100, random_state=42)
+model_dist.fit(X_dist_train, y_dist_train)
+
+model_fuel = RandomForestRegressor(n_estimators=100, random_state=42)
+model_fuel.fit(X_fuel_train, y_fuel_train)
+
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Evaluate
+feature_importances = pd.Series(model_fuel.feature_importances_, index=X_fuel.columns)
+print(feature_importances.sort_values(ascending=False))
+print("\n")
+scores = cross_val_score(model, X, y, cv=5, scoring='r2')
+print("Cross-validated R² scores for CO2 Emissions (5 fold individual):", scores)
+print("Mean R² for CO2 Emissions (5 fold mean):", np.mean(scores))
+print("\n")
+scores = cross_val_score(model_fuel, X, y, cv=5, scoring='r2')
+print("Cross-validated R² scores for Fuel Prediction (5 fold individual):", scores)
+print("Mean R² for Fuel Prediction (5 fold mean):", np.mean(scores))
+print("\n")
+scores = cross_val_score(model_dist, X, y, cv=5, scoring='r2')
+print("Cross-validated R² scores for Distance Prediction (5 fold individual):", scores)
+print("Mean R² for Distance Prediction (5 fold mean):", np.mean(scores))
+
+
+with open("model/ship_dist.pkl", "wb") as file:
+    pickle.dump(model_dist, file)
+with open("model/ship_co2.pkl", "wb") as file:
+    pickle.dump(model, file)
+with open("model/ship_fuel.pkl", "wb") as file:
+    pickle.dump(model_fuel, file)
+
+with open("model/encoder.pkl", "wb") as file:
+    pickle.dump(encoder, file)
+
+with open("model/scaler.pkl", "wb") as file:
+    pickle.dump(scaler, file)
+with open("model/scaler_dist.pkl", "wb") as file:
+    pickle.dump(scaler_dist, file)
+with open("model/scaler_fuel.pkl", "wb") as file:
+    pickle.dump(scaler_fuel, file)
+
+"""
+X = df[['route_id', 'ship_type', 'fuel_type', 'distance', 'fuel_consumption', 'CO2_emissions', 'weather_conditions']]
+y = df['engine_efficiency']
+
+# Encode categorical variables (if not done yet)
+encoder = OrdinalEncoder()
+X[['route_id', 'ship_type', 'fuel_type', 'weather_conditions']] = encoder.fit_transform(
+    X[['route_id', 'ship_type', 'fuel_type', 'weather_conditions']])
+
+# Scale numerical features
+scaler = MinMaxScaler()
+X[['distance', 'fuel_consumption', 'CO2_emissions']] = scaler.fit_transform(
+    X[['distance', 'fuel_consumption', 'CO2_emissions']])
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Create and train a Random Forest model
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Evaluate the model
+score = model.score(X_test, y_test)
+print(f"Model accuracy: {score:.2f}")
+
+new_data = {
+    'route_id': ['Port Harcourt-Lagos'],
+    'ship_type': ['Oil Service Boat'],
+    'fuel_type': ['HFO'],
+    'distance': [128.9],
+    'fuel_consumption': [4461.45],
+    'CO2_emissions': [12779],
+    'weather_conditions': ['Moderate']
+}
+
+new_data_df = pd.DataFrame(new_data)
+new_data_df[['route_id', 'ship_type', 'fuel_type', 'weather_conditions']] = encoder.transform(
+    new_data_df[['route_id', 'ship_type', 'fuel_type', 'weather_conditions']])
+new_data_df[['distance', 'fuel_consumption', 'CO2_emissions']] = scaler.transform(
+    new_data_df[['distance', 'fuel_consumption', 'CO2_emissions']])
+
+predicted_efficiency = model.predict(new_data_df)
+print(f"Predicted engine efficiency: {predicted_efficiency[0]:.2f}")
+"""
